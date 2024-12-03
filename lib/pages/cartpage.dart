2972873e90cdd82a1b5f991e57606a257686e554
coder_Akash <150575760+akashsingh277@users.noreply.github.com>
2024-12-03 23:30:10 +0530

    comleted

--- lib/pages/cartpage.dart
+++ lib/pages/cartpage.dart
@@ -1,11 +1,39 @@
+import 'package:ecommerce_app/models/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../components/cartitem.dart';
+import '../models/shoe.dart';
 class cartPage extends StatelessWidget {
   const cartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('cart'),
-    );
+    return Consumer<Cart>(builder: (context,value,child)=>
+    Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, 
+        children: [ 
+        // heading 
+        const Text('My Cart',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+        ),),
+        
+
+        const SizedBox(height: 20,),
+        Expanded(child: ListView.builder(
+          itemCount:value.getUserCart().length ,
+          itemBuilder:(context,index){
+            // get individual shoe 
+            Shoe individualShoe=value.getUserCart()[index];
+            // return the cart item 
+            return CartItem(shoe: individualShoe,
+            );
+          } ))
+      ],),
+    ),);
   }
 }
